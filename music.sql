508baa92e5713d09bcc21a9ef9ffc3a6dd6dd3b7
leo <leovergarah@gmail.com>
2019-11-02 23:57:08 -0400

    (Updates) to music.sql

--- music.sql
+++ music.sql
@@ -34,5 +34,32 @@ CREATE TABLE songs (
     song_id INTEGER NOT NULL PRIMARY KEY,
     song_name TEXT,
     track_number INT NOT NULL,
+    song_length INT NOT NULL,
     album_id INT NOT NULL
 );
+
+
+INSERT INTO artist
+(artist_id, artist_name)
+VALUES
+(1, "Frank Sinatra"),
+(2, "The Beatles");
+
+INSERT INTO albums
+(album_id, album_name, artist_id)
+VALUES
+(1, 'Abbey Road', 2),
+(2, 'In the Wee Small Hours', 1);
+
+
+INSERT INTO songs 
+(song_id, song_name, track_number, song_length, album_id)
+VALUES
+(1, 'Come Together', 1, 300, 1)
+(2, 'Something', 2, 280, 1)
+(3, 'Maxwells Silver Hammer', 3, 200, 1)
+(4, 'Oh! Darling', 4, 180, 1)
+(7, 'In the Wee Small Hours of the Morning', 1, 260, 2)
+(5, 'Mood Indigo', 2, 190, 2)
+(6, 'Glad to Be Unhappy', 3, 155, 2)
+(8, 'I Get Along Without You Very Well', 4, 205, 2)
