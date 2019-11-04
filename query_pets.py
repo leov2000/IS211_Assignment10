c57790ada105a5879f8eee0203ac6239530f8810
Leo <leovergarah@gmail.com>
2019-11-03 21:48:17 -0500

    (Added) MVP functionality for query

--- query_pets.py
+++ query_pets.py
@@ -25,7 +25,63 @@ def query_db():
     query_result = cursor.fetchall()
     header = [i[0] for i in cursor.description]
 
-    return [*[header], *[query_result]]
+    return [header, query_result]
+
+def value_getter(dict, key, tup):
+    index = dict[key]
+
+    return tup[index]
+
+def print_person_info(tup_info):
+    (name, age) = tup_info
+
+    print(f"{name}, {age} years old")
+
+def print_pet_owner_details(owner, pet_details):
+    print(f"{owner} {pet_details}")
+
+def print_results(query_result, key_dict):
+    head = query_result[0]
+
+    person_name = f"{value_getter(key_dict, 'PersonFirst', head)} {value_getter(key_dict, 'PersonLast', head)}"
+    person_age = f"{value_getter(key_dict, 'PersonAge', head)}"
+
+    print_person_info((person_name, person_age))
+
+    for pet in query_result:
+        pet_info = f"owned {value_getter(key_dict, 'PetName', pet)}, a {value_getter(key_dict, 'PetBreed', pet)}, that was {value_getter(key_dict, 'PetAge', pet)} years old."
+
+        print_pet_owner_details(person_name, pet_info)
+
+# i. Print out data on the person (e.g. James Smith, 41 years old)
+# ii. Print out all the data on that person’s pets (e.g., James Smith owned Rusty, a
+# dalmatian, that was 4 years old)
+
+# [('James', 'Smith', 41, 'Rusty', 'Dalmation', 4, 1, 1, 1), ('James', 'Smith', 41, 'Bella', 'Alaskan Malamute', 3, 0, 1, 2)] 
+    
+
+def find_person(query_results, keys_dict, user_input):
+    person_id = keys_dict['PersonID']
+    filtered_list = filter(lambda x: x[person_id] == user_input, query_results)
+
+    return list(filtered_list)
+    
+
+def get_keys(header_list):
+    config_fields = [
+        'PersonFirst',
+        'PersonLast',
+        'PersonAge',
+        'PetName',
+        'PetBreed',
+        'PetAge',
+        'PetDead',
+        'PersonID',
+        'PetID'
+    ]
+
+    return { k:header_list.index(k) for k in config_fields }
+
 
 def safe_int_checker(int_str):
     """
@@ -41,6 +97,11 @@ def safe_int_checker(int_str):
         return (True, num)
     except ValueError:
         return (False, None)
+
+def print_error(num):
+    print(f'Sorry the personId of {num} does not exist')
+    logging.error(f'Error processing <{num}>')
+
 
 def main():
     """
@@ -58,16 +119,24 @@ def main():
     CLI = True 
 
     while CLI:
-        keyed = input('Ask the user for a person’s ID number\n')
+        keyed = input('\nPlease enter in a numerical ID\n')
         (is_int, cast_num) = safe_int_checker(keyed)
 
         if is_int and cast_num > -1 :
-            print('Query here....')
-            print(cast_num, 'num')
-            results = query_db()
+            db_query = query_db()
+            [header, db_data] = db_query
+            header_index_dict = get_keys(header)
+            plucked_values = find_person(db_data, header_index_dict, cast_num)
+
+            print_error(cast_num) if not plucked_values else print_results(plucked_values, header_index_dict)
+
+        elif is_int == False:
+            print('It looks like you entered a non-numerical key, please enter in a numerical key')
+            logging.error(f'Error processing <{keyed}>')
+
         else:
             CLI = False
-            print('Exit')
+            print('Terminal Exiting....')
             SystemExit
 
 if __name__ == '__main__':
